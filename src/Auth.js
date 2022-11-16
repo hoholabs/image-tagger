8a0022f1d70758c6236e3d8722cff4a437f6ec59
hoholabs <johnpaulhoran@gmail.com>
2022-11-15 21:30:05 -0500

    added functionality for a user to sign in using their google account

--- src/Auth.js
+++ src/Auth.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import { auth } from './firebase';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithRedirect
+} from 'firebase/auth';
 
 function Auth(props) {
+    const provider = new GoogleAuthProvider();
+
     const defaultValues = {
         email: '',
         password: ''
     };
     const [inputValues, setInputValues] = useState(defaultValues);
+    const [signIn, setSignIn] = useState(false);
+    const [signUp, setSignUp] = useState(false);
 
     const handleChange = (event) => {
         console.log(inputValues);
@@ -35,25 +44,94 @@ function Auth(props) {
                 // ..
             });
     };
+
+    const handleSignIn = (event) => {
+        event.preventDefault();
+        console.log(auth);
+        signInWithEmailAndPassword(
+            auth,
+            inputValues.email,
+            inputValues.password
+        )
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                console.log(auth);
+                console.log(user);
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+    };
     return (
         <div>
-            <form name="signUp" onSubmit={handleSignUp}>
-                <input
-                    value={inputValues.email}
-                    onChange={handleChange}
-                    name="email"
-                    type="text"
-                    placeholder="email"
-                ></input>
-                <input
-                    value={inputValues.password}
-                    onChange={handleChange}
-                    type="password"
-                    name="password"
-                    placeholder="password"
-                ></input>
-                <input type="submit" name="signUp" value="Sign up"></input>
-            </form>
+            <button
+                onClick={() => {
+                    signInWithRedirect(auth, provider);
+                }}
+            >
+                googz
+            </button>
+            {!signIn && !signUp && (
+                <div>
+                    <button
+                        onClick={() => {
+                            setSignIn(true);
+                        }}
+                    >
+                        Sign In
+                    </button>
+                    <button
+                        onClick={() => {
+                            setSignUp(true);
+                        }}
+                    >
+                        Sign Up
+                    </button>
+                </div>
+            )}
+            {/* Can probbaly shorten this later
+            Should be able to do signUp and signIn as the same form? */}
+            {signUp && (
+                <form name="signUp" onSubmit={handleSignUp}>
+                    <input
+                        value={inputValues.email}
+                        onChange={handleChange}
+                        name="email"
+                        type="text"
+                        placeholder="email"
+                    ></input>
+                    <input
+                        value={inputValues.password}
+                        onChange={handleChange}
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                    ></input>
+                    <input type="submit" name="signUp" value="Sign up"></input>
+                </form>
+            )}
+            {signIn && (
+                <form name="signIn" onSubmit={handleSignIn}>
+                    <input
+                        value={inputValues.email}
+                        onChange={handleChange}
+                        name="email"
+                        type="text"
+                        placeholder="email"
+                    ></input>
+                    <input
+                        value={inputValues.password}
+                        onChange={handleChange}
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                    ></input>
+                    <input type="submit" name="signIn" value="Sign In"></input>
+                </form>
+            )}
         </div>
     );
 }
